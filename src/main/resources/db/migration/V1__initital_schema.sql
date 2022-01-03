945d5ed7332bc6f99ea7701372a32dccb4cefc77
warlock <warlock@roadtowizardry.com>
2022-01-03 07:07:24 -0600

    initial backend setup docker-compose working flyway working

--- src/main/resources/db/migration/V1__initital_schema.sql
+++ src/main/resources/db/migration/V1__initital_schema.sql
@@ -1,11 +1,23 @@
+-- drop all the tables while testing
+DROP TABLE IF EXISTS containers;
+
+
 CREATE TABLE containers
 (
-    id   serial,
-    type VARCHAR(32) NOT NULL,
-    room varchar(32) NOT NULL,
+    id          serial,
+    name        VARCHAR(32) NOT NULL,
+    type        VARCHAR(32) NOT NULL,
+    room        varchar(32) NOT NULL,
     description varchar(255),
-    height integer,
-    width integer,
-    length integer,
+    height      float,
+    width       float,
+    length      float,
     PRIMARY KEY (id)
 );
+
+INSERT INTO containers(name, type, room, description, height, width, length)
+VALUES ('mil stuff',
+        'box',
+        'closet',
+        'Items for turn in at ETS',
+        14, 17.5, 37)
